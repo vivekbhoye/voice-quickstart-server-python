37ccde3b5f480c476460d37a872f0e77fa8ca736
vivek bhoye <vivekrbhoye@gmail.com>
2022-09-12 17:36:08 +0530

    update server.py

--- server.py
+++ server.py
@@ -45,8 +45,8 @@ def token():
     outgoing_application_sid=app_sid
   )
   # to = request.values.get("to")
-  identity = request.values.get("identity")
-  # identity = request.values["identity"] 
+  # identity = request.values.get("identity")
+  identity = request.values["identity"] 
           # if request.values and request.values["identity"] else IDENTITY
   token = AccessToken(account_sid, api_key, api_key_secret, identity=identity)
   token.add_grant(grant)
